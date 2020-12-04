7a3e5b5b85a47b314f0916144e71c48d269d8ce8
Trisha Gee <trisha.gee@gmail.com>
2020-12-04 16:38:11 +0100

    Data tables test

--- src/test/groovy/com/mechanitis/demo/spock/ExampleSpecification.groovy
+++ src/test/groovy/com/mechanitis/demo/spock/ExampleSpecification.groovy
@@ -47,4 +47,15 @@ class ExampleSpecification extends Specification {
         where:
         sides << [3, 4, 5, 8, 14]
     }
+
+    def "should use data tables for calculating max. Max of #a and #b is #max"() {
+        expect:
+        Math.max(a, b) == max
+
+        where:
+        a | b || max
+        1 | 3 || 3
+        7 | 4 || 7
+        0 | 0 || 0
+    }
 }
