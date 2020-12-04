299195a037889242316bd16610e5021ba6bf0d70
Trisha Gee <trisha.gee@gmail.com>
2020-12-04 16:27:05 +0100

    First data pipe test

--- src/test/groovy/com/mechanitis/demo/spock/ExampleSpecification.groovy
+++ src/test/groovy/com/mechanitis/demo/spock/ExampleSpecification.groovy
@@ -27,4 +27,16 @@ class ExampleSpecification extends Specification {
         def exception = thrown(TooFewSidesException)
         exception.numberOfSides == 0
     }
+
+    def "should expect an Exception to be thrown for invalid input: #sides"() {
+        when:
+        new Polygon(sides)
+
+        then:
+        def exception = thrown(TooFewSidesException)
+        exception.numberOfSides == sides
+
+        where:
+        sides << [-1, 0, 1, 2]
+    }
 }
